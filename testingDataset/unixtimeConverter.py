2f470bdf12515a65f14ce44e41cb6302fb3f40a2
conmylo <k.mylonas@ysoft.gr>
2024-09-23 18:53:31 +0300

    unixtime converter to be used later

---
+++ testingDataset/unixtimeConverter.py
@@ -0,0 +1,23 @@
+import datetime
+
+def get_time_of_day(unix_time):
+    # Convert unix timestamp to datetime
+    dt = datetime.datetime.timestamp(unix_time)
+
+    # Extract the hour from the datetime object
+    hour = dt.hour
+
+    # Categorize the time of day
+    if 5 <= hour < 12:
+        return 'Morning'
+    elif 12 <= hour < 17:
+        return 'Afternoon'
+    elif 17 <= hour < 21:
+        return 'Evening'
+    else:
+        return 'Night'
+
+# Example usage
+unix_time = 169545394  # Example Unix time
+time_of_day = get_time_of_day(unix_time)
+print(time_of_day)
